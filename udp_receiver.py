71e2a59c553149dd508e2e2edf439483cec5532f
Guilherme Farache <gfarache@versatushpc.com.br>
2022-08-14 14:56:26 -0300

    Delete dos arquivos _desafio, renomeando arquivos udp_client e udp_server, criação de diretorio para diferentes inputs. Window_size é do receiver e ele envia pro sender, implementação da finalização do lado do cliente. Falta implementar janela do lado do receiver, e momento de break do while true do receiver

---
+++ udp_receiver.py
@@ -0,0 +1,69 @@
+from socket import *
+from packet import *
+import pickle
+import numpy
+import argparse
+
+PACKET_LOSS_PROBABILITY = 0.0
+ACK_SIZE = 1 # Byte
+ACK_PAYLOAD = ""
+WINDOW_SIZE_REQUEST_CODE = "window_size"
+
+def packet_was_lost(probability : float):
+    result_arr = (numpy.random.choice([0, 1], size=1, p=[1-probability, probability]) == 1)
+    return result_arr[0]
+
+def send_window_size(window_size : int, serverSocket : socket):
+    message_encoded, clientAddress = serverSocket.recvfrom(2048)
+    if message_encoded.decode() == WINDOW_SIZE_REQUEST_CODE:
+        print("Sending window size...")
+        serverSocket.sendto(f"{window_size}".encode(), clientAddress)
+
+def main():
+    serverPort = 12000
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind(("", serverPort))
+
+    parser = argparse.ArgumentParser(description="receiver")
+    parser.add_argument("--window_size", type=int, help="Window size", required=True)
+
+    args = parser.parse_args()
+
+    window_size = args.window_size if args.window_size > 10 else 10
+    print(f"Window size: {window_size}")
+    
+    # Waiting for the client to ask the window size
+    send_window_size(window_size, serverSocket)
+
+    recv_data = ""
+
+    print("The server is ready to receive")
+
+    window_begin = 0 # The first packet to be sent in the pipeline
+    window_end = window_begin + window_size # The last packet to be sent in the pipeline
+    nextseqnum = 0  # Next sequence number to be received
+    while True:
+        packet_dumped, clientAddress = serverSocket.recvfrom(2048)
+        packet = pickle.loads(packet_dumped)
+        
+        if not packet_was_lost(PACKET_LOSS_PROBABILITY):
+            if packet.header.seq_number == nextseqnum:
+                recv_data += packet.payload
+                nextseqnum += 1
+                print(f"Packet #{packet.header.seq_number} received, sending ACK...")
+                ack = Packet(Header(packet.header.destination, packet.header.source, packet.header.seq_number, packet.header.window_size, True), ACK_SIZE, ACK_PAYLOAD)
+                ack_dumped = pickle.dumps(ack)
+                serverSocket.sendto(ack_dumped, clientAddress)
+        else:
+            print(f"Packet with sequence number {packet.header.seq_number} was lost")
+    
+    print("Closing socket...")
+    serverSocket.close()
+
+    print("Writing file...")
+    with open("received_file.txt", "w") as file:
+        file.write(recv_data)
+
+if __name__ == "__main__":
+    main()
+    print("Done!")
---
+++ udp_sender.py
@@ -0,0 +1,134 @@
+"""
+Example of usage:
+    python3 udp_client.py --input_path defalt_input.txt --window_size 10
+"""
+
+from socket import *
+import time
+from packet import *
+import argparse
+import sys
+import math
+import pickle
+
+GREATER_PACKET_SIZE = 1024 # The greater size of the packets is 1024 bytes
+SOURCE = 0
+DESTINATION = 1
+WINDOW_SIZE_REQUEST_CODE = "window_size"
+
+def read_input(input_path : str):
+    with open(input_path, "r") as f:
+        lines = f.readlines()
+        data_str = "".join(lines)
+    
+    return data_str
+
+def calculate_n_packets(input_data : str):
+    header_size = sys.getsizeof(Header(0, 0, 0, 0, False))
+    input_size = sys.getsizeof(input_data)
+    n_packets = math.ceil(input_size / (GREATER_PACKET_SIZE - header_size - 49)) # 49 is the size of the payload list empty
+    return n_packets
+
+def generate_buffer(input_data : str, window_size : int):
+    buffer = []
+    n_packets = calculate_n_packets(input_data)
+
+    for i in range(n_packets):
+        header = Header(SOURCE, DESTINATION, i, window_size)
+        max_payload_size = GREATER_PACKET_SIZE - sys.getsizeof(header) - 49 # 49 is the size of the payload list empty
+        payload = input_data[(i-1)*max_payload_size:i*max_payload_size]
+        packet_size = sys.getsizeof(payload) + sys.getsizeof(header)
+
+        packet = Packet(header, packet_size, payload)
+        buffer.append(packet)
+    
+    print(f"There are {n_packets} packets with 1024 bytes and a packet with {packet_size} bytes to be sent")
+
+    return buffer
+
+def ask_window_size():
+    serverName = "localhost"
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    clientSocket.sendto(WINDOW_SIZE_REQUEST_CODE.encode(), (serverName, serverPort))
+
+    window_size_encoded, serverAddress = clientSocket.recvfrom(2048)
+    window_size = int(window_size_encoded.decode())
+
+    print(f"The receiver window size is: {window_size}")
+    return window_size
+
+def send_data(buffer : list, window_size : int):
+    serverName = "localhost"
+    serverPort = 12000
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+    continue_option = False
+
+    timeout = 0.1 # 1 second
+    window_begin = 0 # The first packet to be sent in the pipeline
+    window_end = window_begin + window_size # The last packet to be sent in the pipeline
+    accumulative_ack = 0 # The last smallest ack received
+
+    while True:   
+        if window_begin == window_end:
+            print("All packets were acked, stopping the process...")
+            break
+
+        # Sending all packets in the window through the pipeline
+        print(f"Sending packets from {window_begin} to {window_end-1} that weren't acked yet")
+        for packet_n in range(window_begin, window_end):           
+            # Pickle the object and send it to the server
+            packet_dumped = pickle.dumps(buffer[packet_n])
+            clientSocket.sendto(packet_dumped, (serverName, serverPort))
+
+            # Starting temporization
+            if packet_n == window_begin:
+                start_timer = time.time()
+
+        while True:
+            ack_dumped, serverAddress = clientSocket.recvfrom(2048)
+            ack = pickle.loads(ack_dumped)
+            print(f"ACK #{ack.header.seq_number} received")
+
+            if ack.header.seq_number == accumulative_ack:
+                accumulative_ack += 1
+                window_begin += 1
+                window_end += 1
+
+                print(f"Accumulative ACK increased to {accumulative_ack}")
+
+                if window_end-1 == len(buffer):
+                    window_end = len(buffer)
+
+                break
+            
+            if not continue_option:
+                option = input("")
+                if option == "continue":
+                    continue_option = True
+            
+            # Timeout verification
+            current_time = time.time()
+            if current_time - start_timer > timeout:
+                break
+        
+    print("Closing socket...")
+    clientSocket.close()
+
+def main():
+    parser = argparse.ArgumentParser(description="sender")
+    parser.add_argument("--input_path", type=str, help="Input path", required=True)
+
+    args = parser.parse_args()
+
+    input_data = read_input(args.input_path)
+    window_size = ask_window_size()
+    buffer = generate_buffer(input_data, window_size)
+
+    send_data(buffer, window_size)
+
+if __name__ == "__main__":
+    main()
+    print("Done!")
